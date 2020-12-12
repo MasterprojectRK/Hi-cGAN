1cec1faa89b3ed5029c7f76e38c9e0a7c41034ca
Ralf <noemail@forget.it>
2020-12-12 03:29:51 +0100

    various adaptations to DNN

--- hicGAN.py
+++ hicGAN.py
@@ -1,10 +1,10 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Conv2D, BatchNormalization, LeakyReLU, Conv2DTranspose, Dropout, ReLU
-import datetime
-import time
+from tensorflow.keras.layers import Conv2D, Conv1D, BatchNormalization, LeakyReLU, Conv2DTranspose, Dropout, ReLU, Flatten, Dense
+import numpy as np
 import os
 import matplotlib.pyplot as plt 
 from tqdm import tqdm
+import utils
 
 #implementation of adapted pix2pix cGAN
 #compare tensorflow tutorial https://www.tensorflow.org/tutorials/generative/pix
@@ -15,9 +15,9 @@ class HiCGAN():
         super().__init__()
 
         self.OUTPUT_CHANNELS = 1
-        self.INPUT_CHANNELS = 3
+        self.INPUT_CHANNELS = 1
         self.INPUT_SIZE = 64
-        self.TARGET_CHANNELS = 1
+        self.NR_FACTORS = 14
         self.LAMBDA = 100
         self.loss_object = tf.keras.losses.BinaryCrossentropy(from_logits=True)
         self.generator_optimizer = tf.keras.optimizers.Adam(2e-4, beta_1=0.5)
@@ -27,9 +27,7 @@ class HiCGAN():
         self.discriminator = self.Discriminator()
 
         self.log_dir=log_dir
-        self.summary_writer = tf.summary.create_file_writer(
-            os.path.join(self.log_dir, "fit/" + datetime.datetime.now().strftime("%Y%m%d-%H%M%S")))
-
+        
         self.checkpoint_dir = os.path.join(self.log_dir, 'training_checkpoints')
         self.checkpoint_prefix = os.path.join(self.checkpoint_dir, "ckpt")
         self.checkpoint = tf.train.Checkpoint(generator_optimizer=self.generator_optimizer,
@@ -37,6 +35,37 @@ class HiCGAN():
                                     generator=self.generator,
                                     discriminator=self.discriminator)
 
+        self.progress_plot_name = os.path.join(self.log_dir, "lossOverEpochs.png")
+
+    def oneD_twoD_conversion(self, nr_filters_list=[1], kernel_width_list=[1], nr_neurons_List=[460,881,1690]):
+        model = tf.keras.Sequential()    
+        model.add(tf.keras.layers.Input(shape=(3*self.INPUT_SIZE, self.NR_FACTORS)))
+    #add 1D convolutions
+        for i, (nr_filters, kernelWidth) in enumerate(zip(nr_filters_list, kernel_width_list)):
+            convParamDict = dict()
+            convParamDict["name"] = "conv1D_" + str(i + 1)
+            convParamDict["filters"] = nr_filters
+            convParamDict["kernel_size"] = kernelWidth
+            convParamDict["activation"] = "sigmoid"
+            convParamDict["data_format"]="channels_last"
+            if kernelWidth > 1:
+                convParamDict["padding"] = "same"
+            model.add(Conv1D(**convParamDict))
+        #flatten the output of the convolutions
+        model.add(Flatten(name="flatten_1"))
+        #add the requested number of dense layers and dropout
+        for i, nr_neurons in enumerate(nr_neurons_List):
+            layerName = "dense_" + str(i+1)
+            model.add(Dense(nr_neurons,activation="relu",kernel_regularizer="l2",name=layerName))
+            layerName = "dropout_" + str(i+1)
+            model.add(Dropout(0.5, name=layerName))
+        #add the output layer (corresponding to a predicted submatrix along the diagonal of a Hi-C matrix)
+        nr_outputNeurons = int(1/2 * self.INPUT_SIZE * (self.INPUT_SIZE + 1)) #always an int, even*odd=even    
+        model.add(Dense(nr_outputNeurons,activation="relu",kernel_regularizer="l2",name="upper_triangle"))
+        model.add(CustomReshapeLayer(self.INPUT_SIZE, name="symmetric_matrix_layer"))
+        model.add(SymmetricFromTriuLayer())
+        model.add(tf.keras.layers.Lambda(lambda z: tf.expand_dims(z, axis=-1)))
+        return model
 
     @staticmethod
     def downsample(filters, size, apply_batchnorm=True):
@@ -65,7 +94,9 @@ class HiCGAN():
 
 
     def Generator(self):
-        inputs = tf.keras.layers.Input(shape=[self.INPUT_SIZE,self.INPUT_SIZE,self.INPUT_CHANNELS], name="factorData")
+        inputs = tf.keras.layers.Input(shape=[3*self.INPUT_SIZE,self.NR_FACTORS], name="factorData")
+
+        twoD_conversion = self.oneD_twoD_conversion()
 
         down_stack = [
             #HiCGAN.downsample(64, 4, apply_batchnorm=False), # (bs, 128, 128, 64)
@@ -96,6 +127,7 @@ class HiCGAN():
                                                 activation='tanh') # (bs, 256, 256, 3)
 
         x = inputs
+        x = twoD_conversion(x)
 
         # Downsampling through the model
         skips = []
@@ -126,9 +158,11 @@ class HiCGAN():
     def Discriminator(self):
         initializer = tf.random_normal_initializer(0., 0.02)
 
-        inp = tf.keras.layers.Input(shape=[self.INPUT_SIZE, self.INPUT_SIZE, self.INPUT_CHANNELS], name='input_image')
-        tar = tf.keras.layers.Input(shape=[self.INPUT_SIZE, self.INPUT_SIZE, self.TARGET_CHANNELS], name='target_image')
-        x = tf.keras.layers.concatenate([inp, tar]) # (bs, 80 80, 3+1=4)
+        inp = tf.keras.layers.Input(shape=[3*self.INPUT_SIZE, self.NR_FACTORS], name='input_image')
+        tar = tf.keras.layers.Input(shape=[self.INPUT_SIZE, self.INPUT_SIZE, self.OUTPUT_CHANNELS], name='target_image')
+        twoD_conversion = self.oneD_twoD_conversion()
+
+        x = tf.keras.layers.concatenate([twoD_conversion(inp), tar]) # (bs, 80 80, 3+1=4)
 
         down1 = HiCGAN.downsample(64, 4, False)(x) # (bs, 128, 128, 64)
         down2 = HiCGAN.downsample(128, 4)(down1) # (bs, 64, 64, 128)
@@ -171,15 +205,23 @@ class HiCGAN():
         self.discriminator_optimizer.apply_gradients(zip(discriminator_gradients,
                                                     self.discriminator.trainable_variables))
 
-        with self.summary_writer.as_default(): #pylint: disable=not-context-manager
-            tf.summary.scalar('gen_total_loss', gen_total_loss, step=epoch)
-            tf.summary.scalar('gen_gan_loss', gen_gan_loss, step=epoch)
-            tf.summary.scalar('gen_l1_loss', gen_l1_loss, step=epoch)
-            tf.summary.scalar('disc_loss', disc_loss, step=epoch)
+        return gen_total_loss, disc_loss
+
+    @tf.function
+    def validationStep(self, input_image, target, epoch):
+        gen_output = self.generator(input_image, training=True)
+
+        disc_real_output = self.discriminator([input_image, target], training=True)
+        disc_generated_output = self.discriminator([input_image, gen_output], training=True)
+
+        gen_total_loss, gen_gan_loss, gen_l1_loss = self.generator_loss(disc_generated_output, gen_output, target)
+        disc_loss = self.discriminator_loss(disc_real_output, disc_generated_output)
+
+        return gen_total_loss, disc_loss
 
     def generate_images(self, model, test_input, tar, epoch: int):
         prediction = model(test_input, training=True)
-        figname = "testpred_epoch_{:05d}.png".format(epoch+1)
+        figname = "testpred_epoch_{:05d}.png".format(epoch)
         figname = os.path.join(self.log_dir, figname)
         display_list = [test_input["factorData"][0], tar["out_matrixData"][0], prediction[0]]
         titleList = ['Input Image', 'Ground Truth', 'Predicted Image']
@@ -191,21 +233,64 @@ class HiCGAN():
         fig1.savefig(figname)
 
     def fit(self, train_ds, epochs, test_ds, steps_per_epoch: int):
+        gen_loss_train = []
+        gen_loss_val = []
+        disc_loss_train =[]
+        disc_loss_val = []
         for epoch in range(epochs):
-            for example_input, example_target in test_ds.take(1):
-                self.generate_images(self.generator, example_input, example_target, epoch)
-
+            #generate sample output
+            if epoch % 5 == 0:
+                for example_input, example_target in test_ds.take(1):
+                    self.generate_images(self.generator, example_input, example_target, epoch)
             # Train
             train_pbar = tqdm(train_ds.enumerate(), total=steps_per_epoch)
             train_pbar.set_description("Epoch {:05d}".format(epoch+1))
+            gen_loss_batches = []
+            disc_loss_batches = []
             for _, (input_image, target) in train_pbar:
-                self.train_step(input_image["factorData"], target["out_matrixData"], epoch)
-
+                gen_loss, disc_loss = self.train_step(input_image["factorData"], target["out_matrixData"], epoch)
+                gen_loss_batches.append(gen_loss)
+                disc_loss_batches.append(disc_loss)
+            gen_loss_train.append(np.mean(gen_loss_batches))
+            disc_loss_train.append(np.mean(disc_loss_batches))
+            # Validation
+            gen_loss_batches = []
+            disc_loss_batches = []
+            for input_image, target in test_ds:
+                gen_loss, disc_loss = self.validationStep(input_image["factorData"], target["out_matrixData"], epoch)
+                gen_loss_batches.append(gen_loss)
+                disc_loss_batches.append(disc_loss)
+            gen_loss_val.append(np.mean(gen_loss_batches))
+            disc_loss_val.append(np.mean(disc_loss_batches))
+            
             # saving (checkpoint) the model every 20 epochs
             if (epoch + 1) % 20 == 0:
                 self.checkpoint.save(file_prefix = self.checkpoint_prefix)
+                #plot loss
+                utils.plotLoss(pLossValueLists=[gen_loss_train, gen_loss_val, disc_loss_train, disc_loss_val], 
+                            pNameList=["gen.loss train", "gen.loss val", "disc.loss train", "disc.loss val"], 
+                            pFilename=self.progress_plot_name,
+                            useLogscale=True)
+                np.savez(os.path.join(self.log_dir, "lossValues_{:05d}.npz".format(epoch)), 
+                                    genLossTrain=gen_loss_train, 
+                                    genLossVal=gen_loss_val, 
+                                    discLossTrain=disc_loss_train, 
+                                    discLossVal=disc_loss_val)
+                self.generator.save(filepath=os.path.join(self.log_dir, "generator_{:05d}.h5".format(epoch)), save_format="h5")
+                self.discriminator.save(filepath=os.path.join(self.log_dir, "discriminator_{:05d}.h5".format(epoch)), save_format="h5")
 
         self.checkpoint.save(file_prefix = self.checkpoint_prefix)
+        utils.plotLoss(pLossValueLists=[gen_loss_train, gen_loss_val, disc_loss_train, disc_loss_val], 
+                            pNameList=["gen.loss train", "gen.loss val", "disc.loss train", "disc.loss val"], 
+                            pFilename=self.progress_plot_name,
+                            useLogscale=True)
+        np.savez(os.path.join(self.log_dir, "lossValues_{:05d}.npz".format(epoch)), 
+                                    genLossTrain=gen_loss_train, 
+                                    genLossVal=gen_loss_val, 
+                                    discLossTrain=disc_loss_train, 
+                                    discLossVal=disc_loss_val)
+        self.generator.save(filepath=os.path.join(self.log_dir, "generator_{:05d}.h5".format(epoch)), save_format="h5")
+        self.discriminator.save(filepath=os.path.join(self.log_dir, "discriminator_{:05d}.h5".format(epoch)), save_format="h5")
 
     def plotModels(self, outputpath: str, figuretype: str):
         generatorPlotName = "generatorModel.{:s}".format(figuretype)
@@ -214,3 +299,53 @@ class HiCGAN():
         discriminatorPlotName = os.path.join(outputpath, discriminatorPlotName)
         tf.keras.utils.plot_model(self.generator, show_shapes=True, to_file=generatorPlotName)
         tf.keras.utils.plot_model(self.discriminator, show_shapes=True, to_file=discriminatorPlotName)
+
+    
+class CustomReshapeLayer(tf.keras.layers.Layer):
+    '''
+    reshape a 1D tensor such that it represents 
+    the upper triangular part of a square 2D matrix with shape (matsize, matsize)
+    #example: 
+     [1,2,3,4,5,6] => [[1,2,3],
+                       [0,4,5],
+                       [0,0,6]]
+    '''
+    def __init__(self, matsize, **kwargs):
+        super(CustomReshapeLayer, self).__init__(**kwargs)
+        self.matsize = matsize
+        self.triu_indices = [ [x,y] for x,y in zip(np.triu_indices(self.matsize)[0], np.triu_indices(self.matsize)[1]) ]
+
+    def call(self, inputs):      
+        return tf.map_fn(self.pickItems, inputs, parallel_iterations=20, swap_memory=True)
+        
+    def pickItems(self, inputVec):
+        sparseTriuTens = tf.SparseTensor(self.triu_indices, 
+                                        values=inputVec, 
+                                        dense_shape=[self.matsize, self.matsize] )
+        return tf.sparse.to_dense(sparseTriuTens)
+
+    def get_config(self):
+        return {"matsize": self.matsize}
+
+class SymmetricFromTriuLayer(tf.keras.layers.Layer):
+    '''
+    make upper triangular tensors symmetric
+    example:
+    [[1,2,3],
+     [0,4,5],
+     [0,0,6]] 
+    becomes:
+    [[1,2,3],
+     [2,4,5],
+     [3,5,6]] 
+    '''
+    def __init__(self, **kwargs):
+        super().__init__(**kwargs)
+
+    def call(self, inputs):
+        return tf.map_fn(self.makeSymmetric, inputs, parallel_iterations=20, swap_memory=True)
+
+    def makeSymmetric(self, inputMat):
+        outMat = inputMat + tf.transpose(inputMat) - tf.linalg.band_part(inputMat, 0, 0)
+        #the diagonal is the same for input and transpose, so subtract it once
+        return outMat
